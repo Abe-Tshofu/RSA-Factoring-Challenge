e1ac524bb67ef11b19ecb94e202106d35e7a9598
Abe-Tshofu <abeetshofu@gmail.com>
2022-12-24 22:12:07 -0800

    factorization.py update

--- factorization.py
+++ factorization.py
@@ -29,4 +29,30 @@ def prime_factorize(n):
         factors.append(n)
     return factors
 
+print(prime_factorize(12345)def prime_factorize(n):
+    factors = []
+    if n <= 3:
+        factors.append(n)
+        return factors
+    x = random.randint(2, n - 1)
+    y = x
+    c = random.randint(1, n - 1)
+    d = 1
+    while d == 1:
+        x = (x * x + c) % n
+        y = (y * y + c) % n
+        y = (y * y + c) % n
+        d = math.gcd(abs(y - x), n)
+        if y == x:
+            d = n
+        if y == x + c:
+            c = random.randint(1, n - 1)
+    if d != n:
+        factors.append(d)
+        factors.append(n // d)
+    else:
+        factors.append(n)
+    return factors
+
 print(prime_factorize(12345))
+)
