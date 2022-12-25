12b9f5f6aace04255c2deeae5a2ec348163a8595
Abe-Tshofu <abeetshofu@gmail.com>
2022-12-24 22:04:12 -0800

    factorization.py

--- factorization.py
+++ factorization.py
@@ -1,21 +1,32 @@
 #/usr/bin/env python3
 import sys
 
+import math
+import random
 
-def primef(n):
+def prime_factorize(n):
+    factors = []
     if n <= 3:
-        return int(n)
-    if n % 2 == 0:
-        return 2
-    elif n % 3 == 0:
-        return 3
+        factors.append(n)
+        return factors
+    x = random.randint(2, n - 1)
+    y = x
+    c = random.randint(1, n - 1)
+    d = 1
+    while d == 1:
+        x = (x * x + c) % n
+        y = (y * y + c) % n
+        y = (y * y + c) % n
+        d = math.gcd(abs(y - x), n)
+        if y == x:
+            d = n
+        if y == x + c:
+            c = random.randint(1, n - 1)
+    if d != n:
+        factors.append(d)
+        factors.append(n // d)
     else:
-        for i in range(5, int((n)**0.5) + 1, 6):
-            if n % i == 0:
-                return int(i)
-            if n % (i + 2) == 0:
-                return primef(n/(i+2))
-    return int(n)
+        factors.append(n)
+    return factors
 
-
-print(primef(int(sys.argv[1])))
+print(prime_factorize(12345))
